22a44ebd58913caa909177120b941f9e6ca4f935
Eamonn de Leastar <edeleastar@wit.ie>
2023-01-31 09:04:20 +0000

    roll back

--- src/models/mem/playlist-mem-store.js
+++ src/models/mem/playlist-mem-store.js
@@ -16,11 +16,8 @@ export const playlistMemStore = {
 
   async getPlaylistById(id) {
     const list = playlists.find((playlist) => playlist._id === id);
-    if (list) {
-      list.tracks = await trackMemStore.getTracksByPlaylistId(list._id);
-      return list;
-    }
-    return null;
+    list.tracks = await trackMemStore.getTracksByPlaylistId(list._id);
+    return list;
   },
 
   async getUserPlaylists(userid) {
@@ -29,7 +26,7 @@ export const playlistMemStore = {
 
   async deletePlaylistById(id) {
     const index = playlists.findIndex((playlist) => playlist._id === id);
-    if (index !== -1) playlists.splice(index, 1);
+    playlists.splice(index, 1);
   },
 
   async deleteAllPlaylists() {
