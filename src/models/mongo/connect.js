844a1bdb9e309812c4c7d8139c443d6f23457b35
Eamonn de Leastar <edeleastar@wit.ie>
2023-01-31 10:26:20 +0000

    0.10.1

--- src/models/mongo/connect.js
+++ src/models/mongo/connect.js
@@ -1,12 +1,21 @@
 import * as dotenv from "dotenv";
 import Mongoose from "mongoose";
+import * as mongooseSeeder from "mais-mongoose-seeder";
+import { seedData } from "./seed-data.js";
+
+const seedLib = mongooseSeeder.default;
+
+async function seed() {
+  const seeder = seedLib(Mongoose);
+  const dbData = await seeder.seed(seedData, { dropDatabase: false, dropCollections: true });
+  console.log(dbData);
+}
 
 export function connectMongo() {
   dotenv.config();
 
   Mongoose.set("strictQuery", true);
   Mongoose.connect(process.env.db);
-
   const db = Mongoose.connection;
 
   db.on("error", (err) => {
@@ -17,8 +26,8 @@ export function connectMongo() {
     console.log("database disconnected");
   });
 
-  // eslint-disable-next-line func-names
   db.once("open", function () {
     console.log(`database connected to ${this.name} on ${this.host}`);
+    seed();
   });
 }
